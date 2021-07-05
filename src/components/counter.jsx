63e1270caeefc76d4ac5d041ab73c5c69a52f7d3
eriysd <74118903+eriysd@users.noreply.github.com>
2021-07-05 11:39:36 -0400

    updated counter component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Counter extends Component {
-    render() { 
-        return <h1>Hello World</h1>;
-    }
+  state = {
+    count: 0,
+  };
+
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold",
+  };
+
+  render() {
+    return (
+      <div>
+        {/*using React.Fragment gets rid of extra div */}
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </div>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
- 
 export default Counter;
